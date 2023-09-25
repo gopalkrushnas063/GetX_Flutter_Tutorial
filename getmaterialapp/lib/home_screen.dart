89ee1889fdf204278f21f55d730993d882cc0c45
WAYINDIA <developers@wayindia.com>
2023-09-25 13:33:36 +0530

    Using GetX add bottom sheet and change theme mode

--- getmaterialapp/lib/home_screen.dart
+++ getmaterialapp/lib/home_screen.dart
@@ -33,6 +33,40 @@ class _HomeScreenState extends State<HomeScreen> {
               },
             ),
           ),
+          Card(
+            child: ListTile(
+              title: Text('GetX Bottom Sheet'),
+              subtitle: Text('GetX Dialogue Alert With GetX'),
+              onTap: () {
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.amber,
+                    ),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Get.changeThemeMode(ThemeMode.light);
+                          },
+                          leading:const Icon(Icons.light_mode),
+                          title:const Text('Light Theme'),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Get.changeThemeMode(ThemeMode.dark);
+                          },
+                          leading: const Icon(Icons.dark_mode),
+                          title: const Text('Dark Theme'),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
